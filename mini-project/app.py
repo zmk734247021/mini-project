e5144bed8f53b41c08d497c4593c0e3fce011100
zmk734247021 <49070449+zmk734247021@users.noreply.github.com>
2019-03-29 18:29:36 +0000

    Update app.py

--- mini-project/app.py
+++ mini-project/app.py
@@ -17,29 +17,28 @@ def hello(mydata):
     if res.ok:
         crimes = res.json()
     else:
-        print(res.reason)
+        print(res.reason)                  #mydata
     cat_url_template = 'https://data.police.uk/api/crime-categories?date={date}'
     res = requests.get(cat_url_template.format(date = mydata))
     if res.ok:
         cate_json = res.json()
     else:
-        print(res.reason)
+        print(res.reason)#find the categories
 
     cat = {categ["url"]:categ["name"] for categ in cate_json}
     crime_cate_stats = dict.fromkeys(cat.keys(), 0)
-    crime_cate_stats.pop("all-crime")
+    crime_cate_stats.pop("all-crime")#delete all-crime
     for crime in crimes:
         crime_cate_stats[crime["cat"]] += 1
     for key in crime_cate_stats:
-        rows = session.execute( """insert into crime.category(month,name,count) values('{}','{}',{})""".format(mydata,key,crime_cate_stats[key]))
+        rows = session.execute( """insert into crime.cate(month,name,count) values('{}','{}',{})""".format(mydata,key,crime_cate_stats[key]))
     return('<h1>crime data for {} is created</h1>'.format(mydata))
 
-@app.route('/<data>/<name>', methods=['GET'])
-def profile(data,name):
-    rows = session.execute( """select * from crime.category where month = '{}' and name= '{}' allow filtering""".format(data,name,))
+@app.route('/<time>/<name>', methods=['GET'])
+def profile(time,name):
+    rows = session.execute( """select * from crime.cate where month = '{}' and name= '{}' allow filtering""".format(time,name))
     for crime in rows:
-        return jsonify(crime)
-        # return('<h1>{} in {} happened {} times!</h1>'.format(name, data, crime.count))
+        return jsonify(crime)#return as a json format 
     return('<h1>That kind of crime does not exist!</h1>')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
